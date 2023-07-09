e5d2a5a47e0e5b68e38c0b80815cf5ec02ca7815
Prabhjot Singh <68184118+777Prabhjot@users.noreply.github.com>
2023-07-09 15:39:28 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    return res.send('hello prabhjot')
+    return res.send('hello prabhjot how are you')
 });
 
 app.listen(5000, () => {
